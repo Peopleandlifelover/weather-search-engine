48efae04dd6c8482f7d84db92b1df42948b60cd0
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2022-12-30 22:57:55 +0100

    added redirect to a new page

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ root.render(
         <h1>Weather Search Engine</h1>
         <WeatherSearch />
         <footer>
-          <a href="https://github.com/Natta-Sha/weather-search-engine">
+          <a
+            href="https://github.com/Natta-Sha/weather-search-engine"
+            target="_blank"
+          >
             My git repository
           </a>
         </footer>
